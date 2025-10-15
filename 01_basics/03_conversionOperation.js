2c0976d37b47ae9bee770cf2bc620c3c3bbe6452
Ali_Torab <a.torab@blueskyautomation.global>
2025-10-15 17:32:15 +0500

    Conversion Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,14 @@
-let num = 23;
-console.log("Number:", num, "| Type:", typeof num);
+let numVar = 23;
+console.log("Number:", numVar, "| Type:", typeof numVar);
 
-let strNum = String(num);
+let strNum = String(numVar);
 console.log("Number changed to string:", strNum, "| Type:", typeof strNum);
+
+console .log("--------------------------------------------------");
 
 let nullVar = 0;
 console.log("Null variable:", nullVar, "| Type:", typeof nullVar);
 
-let bool
+let boolNull = Boolean(nullVar);
+console.log("Null changed to Booean:", boolNull, "| Type:", typeof boolNull);
+
