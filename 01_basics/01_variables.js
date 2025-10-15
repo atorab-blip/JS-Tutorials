37ac41339c4d8293cec55e350100523ac6f3037b
Ali_Torab <a.torab@blueskyautomation.global>
2025-10-15 15:34:33 +0500

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -9,8 +9,8 @@ console.log("Accout Password:", accountPassword);
 console.log("Accout City:", accountCity);
 
 // accountID = 234567; // error: Assignment to constant variable.
-accountEmail = "test@gmail.com"; // valid
-accountPassword = "12345"; // valid
+accountEmail = "tut@gmail.com"; // valid
+accountPassword = "00000"; // valid
 accountCity = "Los Angeles"; // valid  
 
 console.log("\n\t--- After changes: ---");            //use \n for line shift and \t for tab space
@@ -18,3 +18,13 @@ console.log("Accout Id:", accountID, );
 console.log("Accout Email:", accountEmail);
 console.log("Accout Password:", accountPassword);
 console.log("Accout City:", accountCity);
+
+console.table({accountID, accountEmail, accountPassword, accountCity}); //displays data in table format
+
+console.info("Account Info Printed Successfully!");
+console.warn("This is a warning message!");
+console.debug("This is a debug message!");
+console.assert(4 === 3, "4 is not equal to 3"); //assertion failed message will be printed
+
+// console.error("This is an error message!");
+// console.clear(); //clears the console
