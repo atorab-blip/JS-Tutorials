60db7b17ae1097ca6918c56000f564b26163120e
Ali_Torab <a.torab@blueskyautomation.global>
2025-10-15 15:26:36 +0500

    container.config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,3 +4,17 @@ var accountPassword = "1234";
 accountCity = "New York";
 
 console.log("Accout Id:", accountID, );
+console.log("Accout Email:", accountEmail);
+console.log("Accout Password:", accountPassword);
+console.log("Accout City:", accountCity);
+
+// accountID = 234567; // error: Assignment to constant variable.
+accountEmail = "test@gmail.com"; // valid
+accountPassword = "12345"; // valid
+accountCity = "Los Angeles"; // valid  
+
+console.log("\n\t--- After changes: ---");            //use \n for line shift and \t for tab space
+console.log("Accout Id:", accountID, );
+console.log("Accout Email:", accountEmail);
+console.log("Accout Password:", accountPassword);
+console.log("Accout City:", accountCity);
